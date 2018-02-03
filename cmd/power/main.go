9372e4ad9481c82b53f6083b5ab2c9125961769b
Joshua Sjoding <joshua.sjoding@scjalliance.com>
2018-02-02 16:52:05 -0800

    Switched to signaler library for interrupt handling

--- cmd/power/main.go
+++ cmd/power/main.go
@@ -1,13 +1,14 @@
 package main
 
 import (
-	"context"
 	"flag"
 	"fmt"
 	"os"
 	"strings"
+	"syscall"
 	"time"
 
+	"github.com/gentlemanautomaton/signaler"
 	"github.com/scjalliance/power"
 	"github.com/scjalliance/power/consolerecipient"
 	"github.com/scjalliance/power/stathatrecipient"
@@ -20,6 +21,10 @@ const (
 )
 
 func main() {
+	shutdown := signaler.New().Capture(os.Interrupt, syscall.SIGTERM)
+	defer shutdown.Wait()
+	defer shutdown.Trigger()
+
 	var (
 		sourceStr     = os.Getenv("SOURCE")
 		statisticsStr = os.Getenv("STATISTICS")
@@ -99,9 +104,7 @@ func main() {
 		}
 	}
 
-	shutdown := NewShutdown()
-
-	execute(sources, stats, recipients, verbose, shutdown)
+	execute(shutdown.Signal, sources, stats, recipients, verbose)
 
 	if interval > 0 {
 		ticker := time.NewTicker(interval)
@@ -109,17 +112,24 @@ func main() {
 		for {
 			select {
 			case <-ticker.C:
-				execute(sources, stats, recipients, verbose, shutdown)
-			case <-shutdown:
+				execute(shutdown.Signal, sources, stats, recipients, verbose)
+			case <-shutdown.Signal:
 				return
 			}
 		}
 	}
 }
 
-func execute(sources []power.Source, stats []power.Statistic, recipients []power.Recipient, verbose bool, shutdown Shutdown) {
-	ctx, cancel := shutdown.WithContext(context.Background())
-	defer cancel()
+func execute(shutdown signaler.Signal, sources []power.Source, stats []power.Statistic, recipients []power.Recipient, verbose bool) {
+	if shutdown.Signaled() {
+		return
+	}
+
+	stop := shutdown.Derive()
+	defer stop.Wait()
+	defer stop.Trigger()
+
+	ctx := stop.Context()
 
 	for i, source := range sources {
 		for _, r := range recipients {
